e943de5d6ef03b5ac0ffa981ad62797740e6dfcc
P. Isaac Hildebrandt <isaac@pihimedia.com>
2017-10-02 13:03:33 -0500

    test init script

--- tools/TestFileNuGetPackage.Init.ps1
+++ tools/TestFileNuGetPackage.Init.ps1
@@ -1,2 +1,10 @@
 param($installPath, $toolsPath, $package)
 
+$rootPath = (Get-Item $installPath).Parent.Parent.FullName
+$sourcePath = "$installPath\deploy"
+$targetPath = "$rootPath\"
+
+Write-Host "InstallPath: $installPath"
+Write-Host "ToolsPath: $toolsPath"
+Write-Host "SourcePath: $sourcePath" 
+Write-Host "TargetPath: $targetPath"
