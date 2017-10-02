39f90899e44c4beed4426d3fffc9c5a719403412
P. Isaac Hildebrandt <isaac@pihimedia.com>
2017-10-02 13:34:19 -0500

    destination

--- tools/TestFileNuGetPackage.Init.ps1
+++ tools/TestFileNuGetPackage.Init.ps1
@@ -6,11 +6,15 @@ param(
 
 $rootPath = (Get-Item $installPath).Parent.Parent.FullName
 $sourcePath = "$installPath\deploy"
+$destinationPath = Split-Path -Path $project.FullName
+
+Write-Host "DestinationPath: $destinationPath"
 
 Get-ChildItem -Path $sourcePath -Filter "*.zip" | 
     ForEach-Object {
         $fullName = $_.FullName
         $name = $_.BaseName
-
+        
         Expand-Archive -Path $fullName -DestinationPath "$sourcePath\$name"
+        
     }
